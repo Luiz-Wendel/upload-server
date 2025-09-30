8ad261f1c3915a38038e9ea48e4425d98e1e4b31
Luiz Wendel <alexandre.lawb@gmail.com>
2025-09-29 22:44:20 -0300

    feat: add swagger config

--- src/infra/http/routes/upload-image.ts
+++ src/infra/http/routes/upload-image.ts
@@ -1,9 +1,28 @@
-import type { 
-  FastifyPluginAsyncZod
-} from "fastify-type-provider-zod"
+import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod';
+import z from 'zod';
 
-export const uploadImageRoute: FastifyPluginAsyncZod = async (server) => {
-  server.post('/upload', async () => {
-    return 'Hello World'
-  })
-}
+export const uploadImageRoute: FastifyPluginAsyncZod = async server => {
+  server.post(
+    '/uploads',
+    {
+      schema: {
+        summary: 'Upload image',
+        description: 'Upload an image to the server',
+        tags: ['Upload'],
+        body: z.object({
+          name: z.string(),
+          password: z.string().optional(),
+        }),
+        response: {
+          201: z.object({ uploadId: z.string() }),
+          409: z
+            .object({ message: z.string() })
+            .describe('upload already exists.'),
+        },
+      },
+    },
+    async (_request, reply) => {
+      return reply.status(201).send({ uploadId: 'test' });
+    }
+  );
+};
--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -1,16 +1,27 @@
-import fastify from "fastify";
-import fastifyCors from "@fastify/cors";
-import { serializerCompiler, validatorCompiler, hasZodFastifySchemaValidationErrors } from "fastify-type-provider-zod"
+import fastifyCors from '@fastify/cors';
+import fastifyMultipart from '@fastify/multipart';
+import fastifySwagger from '@fastify/swagger';
+import fastifySwaggerUi from '@fastify/swagger-ui';
+import fastify from 'fastify';
+import {
+  hasZodFastifySchemaValidationErrors,
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod';
+
+// env
+import { env } from '@/env';
 
 // Routes
-import { uploadImageRoute } from "./routes/upload-image";
+import { uploadImageRoute } from './routes/upload-image';
 
 const server = fastify();
 
 server.setValidatorCompiler(validatorCompiler);
 server.setSerializerCompiler(serializerCompiler);
 
-server.setErrorHandler((error, request, reply) => {
+server.setErrorHandler((error, _request, reply) => {
   if (hasZodFastifySchemaValidationErrors(error)) {
     return reply.status(400).send({
       message: 'Validation error',
@@ -27,11 +38,29 @@ server.register(fastifyCors, {
   origin: '*',
 });
 
+server.register(fastifyMultipart);
+
+server.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'Upload Server',
+      description: 'File upload server',
+      version: '1.0.0',
+    },
+  },
+  transform: jsonSchemaTransform,
+});
+server.register(fastifySwaggerUi, {
+  routePrefix: '/docs',
+});
+
 server.register(uploadImageRoute);
 
-server.listen({
-  port: 3333,
-  host: '0.0.0.0',
-}).then(() => {
-  console.log('HTTP server running!');
-});
+server
+  .listen({
+    port: env.PORT,
+    host: '0.0.0.0',
+  })
+  .then(() => {
+    console.log('HTTP server running!');
+  });
