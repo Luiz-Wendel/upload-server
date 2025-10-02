03bb155a7c3d7017c878e04bb176822a805d6241
Luiz Wendel <alexandre.lawb@gmail.com>
2025-10-01 21:15:36 -0300

    feat: add final db configs

--- src/infra/db/schemas/uploads.ts
+++ src/infra/db/schemas/uploads.ts
@@ -1,9 +1,10 @@
 import { pgTable, text, timestamp } from 'drizzle-orm/pg-core';
+import { uuidv7 } from 'uuidv7';
 
 export const uploads = pgTable('uploads', {
   id: text('id')
     .primaryKey()
-    .$defaultFn(() => crypto.randomUUID()),
+    .$defaultFn(() => uuidv7()),
   name: text('name').notNull(),
   remoteKey: text('remote_key').notNull().unique(),
   remoteUrl: text('remote_url').notNull(),
