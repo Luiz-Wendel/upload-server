2044523c94bbcb646fac3312bc822c40c8c1f82c
Luiz Wendel <alexandre.lawb@gmail.com>
2025-10-08 11:06:55 -0300

    feat: get uploads service

---
+++ src/app/services/get-uploads.ts
@@ -0,0 +1,75 @@
+import { asc, count, desc, ilike } from 'drizzle-orm';
+import { z } from 'zod';
+// db
+import { db } from '@/infra/db';
+import { schema } from '@/infra/db/schemas';
+// utils
+import { type Either, makeRight } from '@/utils/either';
+
+const getUploadInput = z.object({
+  searchQuery: z.string().optional(),
+  sortBy: z.enum(['createdAt']).optional(),
+  sortDirection: z.enum(['asc', 'desc']).optional(),
+  page: z.number().optional().default(1),
+  pageSize: z.number().optional().default(20),
+});
+
+type GetUploadInput = z.input<typeof getUploadInput>;
+
+type GetUploadOutput = {
+  uploads: {
+    id: string;
+    name: string;
+    remoteKey: string;
+    remoteUrl: string;
+    createdAt: Date;
+  }[];
+  total: number;
+};
+
+export async function getUpload(
+  input: GetUploadInput
+): Promise<Either<never, GetUploadOutput>> {
+  const { searchQuery, sortBy, sortDirection, page, pageSize } =
+    getUploadInput.parse(input);
+
+  const [uploads, [{ total }]] = await Promise.all([
+    db
+      .select({
+        id: schema.uploads.id,
+        name: schema.uploads.name,
+        remoteKey: schema.uploads.remoteKey,
+        remoteUrl: schema.uploads.remoteUrl,
+        createdAt: schema.uploads.createdAt,
+      })
+      .from(schema.uploads)
+      .where(
+        searchQuery ? ilike(schema.uploads.name, `%${searchQuery}%`) : undefined
+      )
+      .orderBy(fields => {
+        if (sortBy && sortDirection === 'asc') {
+          return asc(fields[sortBy]);
+        }
+
+        if (sortBy && sortDirection === 'desc') {
+          return desc(fields[sortBy]);
+        }
+
+        return desc(fields.id);
+      })
+      .offset((page - 1) * pageSize)
+      .limit(pageSize),
+
+    db
+      .select({ total: count(schema.uploads.id) })
+      .from(schema.uploads)
+      .where(
+        searchQuery ? ilike(schema.uploads.name, `%${searchQuery}%`) : undefined
+      ),
+  ]);
+
+  return makeRight({
+    uploads,
+    total,
+  });
+}
